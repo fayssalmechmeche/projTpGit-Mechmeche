0bccd54b7906fea1c2228fb718835855f066821c
Kenthyvuth <79855940+Kenthyvuth@users.noreply.github.com>
2021-11-16 16:29:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Ceci est une erreur de Tan qui a remplacé une ligne';
+        ?>
+    </body>
+</html>
