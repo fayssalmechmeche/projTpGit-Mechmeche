fb211d27f65a467a3f19d1068a20879b0d060e7d
fayssal <fayssalmechmeche.pro@gmail.com>
2021-11-22 17:00:39 +0100

    Commit du fichier Mechmeche par l'utilisateur Mechmeche

---
+++ presenter.php
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'page web de Mechmeche';
+        ?>
+    </body>
+</html>
